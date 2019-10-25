b570d0e3adfb5e7e093c0b84b14bce6083a8421c
icslabs <ics.labs18@gmail.com>
2019-10-25 22:00:55 +0800

    fresh Cart

---
+++ lib/model/fruits_data.dart
@@ -0,0 +1,50 @@
+final List myItems = [
+  {
+    "src": "assets/buah/anggur.jpg",
+    "name": "Anggur",
+    "desc": "Buah Anggur Segar",
+    "price": "20.000"
+  },
+  {
+    "src": "assets/buah/apple.jpg",
+    "name": "Apel",
+    "desc": "Buah Apel Segar",
+    "price": "25.000"
+  },
+  {
+    "src": "assets/buah/banana.jpg",
+    "name": "Pisang",
+    "desc": "Buah Pisang Segar",
+    "price": "22.000"
+  },
+  {
+    "src": "assets/buah/langsat.png",
+    "name": "Langsat",
+    "desc": "Buah Langsat Segar",
+    "price": "30.000"
+  },
+  {
+    "src": "assets/buah/naga.png",
+    "name": "Naga",
+    "desc": "Buah Naga Segar",
+    "price": "23.000"
+  },
+  {
+    "src": "assets/buah/orange.jpg",
+    "name": "Jeruk",
+    "desc": "Buah Jeruk Segar",
+    "price": "10.000"
+  },
+  {
+    "src": "assets/buah/rambutan.jpg",
+    "name": "rambutan",
+    "desc": "Buah Rambutan Segar",
+    "price": "40.000"
+  },
+  {
+    "src": "assets/buah/strawberry.jpg",
+    "name": "StrawBerry",
+    "desc": "WBuah StrawBerryortel Segar",
+    "price": "20.000"
+  }
+];
